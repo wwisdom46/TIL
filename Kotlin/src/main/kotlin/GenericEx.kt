aa8f4432fd9fb71a535a79fc4be33ad4da8270d9
wwisdom46 <wisdom1163@gmail.com>
2021-12-02 22:29:23 +0900

    [TIL-14] Kotlin #19,20 Generic, List

---
+++ Kotlin/src/main/kotlin/GenericEx.kt
@@ -0,0 +1,35 @@
+fun main() {
+    UsingGeneric(A()).doShouting()
+    UsingGeneric(B()).doShouting()
+    UsingGeneric(C()).doShouting()
+
+    doShouting(B()) // 캐스팅 없이 B의 객체 그대로 함수에서 사
+}
+
+fun <T: A> doShouting(t: T) {
+     t.shout()
+}
+
+open class A {
+    open fun shout() {
+        println("A shout")
+    }
+}
+
+class B : A() {
+    open fun shout() {
+        println("B shout")
+    }
+}
+
+ class C : A() {
+    open fun shout() {
+        println("A shout")
+    }
+}
+
+class UsingGeneric<T: A> (val t: T){
+    fun doShouting() {
+        t.shout()
+    }
+}
