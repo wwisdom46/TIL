1f3ec24c5cf268ceec3a93e13891f88227cb271a
wisdom46 <wisdom1163@gmail.com>
2021-12-17 23:21:12 +0900

    [TIL-26] Kotlin #26,27 컬렉션 함수

---
+++ Kotlin/src/main/kotlin/MapEx.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val a = mutableMapOf("레드벨벳" to "음파",
+        "트와이스" to "FANCY",
+        "ITZY"  to "ICY")
+    for (entry in a) {
+        println("${entry.key} : ${entry.value}")
+    }
+
+    a.put("오마이걸", "번지")
+    println(a)
+
+    a.remove("ITZY")
+    println(a)
+
+    println(a["레드벨벳"])
+}
