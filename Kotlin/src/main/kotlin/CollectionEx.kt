0ef2f30cf7a85378afb06e5c668fa095b0d44e17
wisdom46 <wisdom1163@gmail.com>
2021-12-21 22:33:18 +0900

    [TIL-28] Kotlin #28 컬렉션 함수-2

--- Kotlin/src/main/kotlin/CollectionEx.kt
+++ Kotlin/src/main/kotlin/CollectionEx.kt
@@ -12,4 +12,15 @@ fun main () {
     val (over98, under98) = personList.partition { it.birthYear > 1998 }
     println(over98)
     println(under98)
+
+    val numbers = listOf(-3, 7, 2, -10, 1)
+
+    println(numbers.flatMap { listOf(it * 10, it + 10) }) // [-30, 7, 70, 17, 20, 12, -100, 0, 10, 11]
+
+    println(numbers.getOrElse(1) { 50 }) // 7
+    println(numbers.getOrElse(10) { 50 }) // 50
+
+    val names = listOf("A", "B", "C", "D")
+
+    println(names zip numbers) // [(A, -3), (B, 7), (C, 2), (D, -10)]
 }
