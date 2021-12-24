19214a84dd606fba481b8f1cccff8ef57d623433
wisdom46 <wisdom1163@gmail.com>
2021-12-24 21:52:08 +0900

    [TIL-29] MSA Spring Cloud Gateway Service - Load Balancer

--- tutorial-msa/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ tutorial-msa/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,10 +1,14 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 // http://localhost:8081/welcome
 // http://localhost:8081/first-service/welcome
@@ -12,6 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
@@ -23,7 +35,10 @@ public class FirstServiceController {
         return "Hello World in First Service.";
     }
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("Hi, there. This is a message from First Service on PORT %s."
+                , env.getProperty("local.server.port"));
     }
 }
