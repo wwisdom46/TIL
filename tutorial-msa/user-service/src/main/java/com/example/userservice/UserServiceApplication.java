7719141ce963576856b3a841590250d04a1133e4
wisdom46 <wisdom1163@gmail.com>
2021-12-27 22:44:44 +0900

    MSA User Microservice - welcome() 메소드, h2 DB 연동

--- tutorial-msa/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ tutorial-msa/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,8 +1,10 @@
 package com.example.userservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
