63a6532b0e2896f557d5e192e757d00ea1968e05
wisdom46 <wisdom1163@gmail.com>
2022-02-19 00:12:19 +0900

    [MSA] Users Microservice - loadUserByUsername() 구현

--- tutorial-msa/user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
+++ tutorial-msa/user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
@@ -4,7 +4,6 @@ package com.example.userservice.jpa;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
-
-
     UserEntity findByUserId(String userId);
+    UserEntity findByEmail(String username);
 }
--- tutorial-msa/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ tutorial-msa/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,15 +1,29 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -27,5 +41,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         authenticationFilter.setAuthenticationManager(authenticationManager());
 
         return authenticationFilter;
+    }
+
+    // select pwd from users where email=?
+    // db_pwd(encrypted) == input_pwd(encrypted)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
