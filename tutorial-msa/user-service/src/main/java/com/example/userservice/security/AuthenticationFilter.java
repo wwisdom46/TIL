391f69e6309e050ae1ca0266861799ff2b142261
wisdom46 <wisdom1163@gmail.com>
2022-02-19 01:05:36 +0900

    [MSA] Users Microservice - Routes 정보 변경

--- tutorial-msa/user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ tutorial-msa/user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -39,6 +39,6 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        //super.successfulAuthentication(request, response, chain, authResult);
     }
 }
