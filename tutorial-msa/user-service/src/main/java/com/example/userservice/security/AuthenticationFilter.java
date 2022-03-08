cc1a7c63e7cec2332794650761cac2d01f4bb45e
wisdom46 <wisdom1163@gmail.com>
2022-03-08 23:19:23 +0900

    [MSA] Users Microservice - 로그인 처리 과정

--- tutorial-msa/user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ tutorial-msa/user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -2,9 +2,11 @@ package com.example.userservice.security;
 
 import com.example.userservice.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
+@Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
@@ -39,6 +42,6 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
-        //super.successfulAuthentication(request, response, chain, authResult);
+        log.debug( ((User)authResult.getPrincipal()).getUsername());
     }
 }
